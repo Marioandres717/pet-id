cd0bc623a41ae4f3ab27c3d2b1e28deae5e315f4
Mario Andres <marioarendon2@gmail.com>
2020-10-17 14:47:11 -0600

    add dashboard page

---
+++ pet-id-app/src/pages/dashboard.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react"
+
+import Layout from "../components/layout"
+
+const Dashboard = () => {  
+  return (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>We`lcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    </div>
+  </Layout>)
+}
+
+export default Dashboard
--- pet-id-app/src/pages/index.js
+++ pet-id-app/src/pages/index.js
@@ -1,18 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
 
-import Layout from "../components/layout"
-
-const IndexPage = () => (
+export default () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>This App rules!</h1>
+    Log in to find out why
+    <Link to="/dashboard">Go to the dashboard</Link>
   </Layout>
-)
-
-export default IndexPage
+);
