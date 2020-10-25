24b25268e806b373e78cf75af20bda734326340e
Mario Andres <marioarendon2@gmail.com>
2020-10-25 02:56:10 -0600

    Add login component

--- pet-id-app/src/components/login.js
+++ pet-id-app/src/components/login.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { useIdentityContext } from "react-netlify-identity";
-import { navigate } from "gatsby";
+import React from "react"
+import { useIdentityContext } from "react-netlify-identity"
+import { navigate } from "gatsby"
 
 const Login = ({ showModal }) => {
-  const identity = useIdentityContext();
+  const identity = useIdentityContext()
 
   if (identity && identity.isLoggedIn) {
-    navigate("/dashboard", { replace: true });
+    navigate("/dashboard/home", { replace: true })
   }
 
   return (
     <>
-      <h1>Log in on Sign up</h1>
+      <h1>Log in or Sign up</h1>
       <button onClick={showModal}>Log In</button>
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
