924f14685401764c0305fca148441c0f748909c6
Mario Andres <marioarendon2@gmail.com>
2020-10-25 03:08:12 -0600

    Add private routes to dashboard

--- pet-id-app/src/pages/dashboard.js
+++ pet-id-app/src/pages/dashboard.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { Router } from "@reach/router"
+import { navigate } from "gatsby"
 import IdentityModal from "react-netlify-identity-widget"
 
 import "react-netlify-identity-widget/styles.css"
+import Login from "../components/login"
+import Home from "../components/home"
+import PrivateRoute from "../components/private-route"
 
-const Dashboard = () => {
+const Dashboard = ({ location }) => {
   const [isVisible, setVisibility] = useState(false)
   const showModal = () => setVisibility(true)
 
+  useEffect(() => {
+    if (location.pathname.match(/^\/dashboard\/?$/)) {
+      navigate("/dashboard/login", { replace: true })
+    }
+  }, [location.pathname])
+
   return (
     <>
-      <h1>Hi people</h1>
-      <p>We`lcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
+      <Router>
+        <Login path="/dashboard/login" showModal={showModal} />
+        <PrivateRoute path="/dashboard/home" component={Home} />
+      </Router>
       <IdentityModal
         showDialog={isVisible}
         onCloseDialog={() => setVisibility(false)}
