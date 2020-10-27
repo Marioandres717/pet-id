f24e010b3036f0fb066787b7aeb204fb442a01f9
Mario Andres <marioarendon2@gmail.com>
2020-10-26 23:41:33 -0600

    footer styles

--- pet-id-app/src/components/layout.js
+++ pet-id-app/src/components/layout.js
@@ -23,11 +23,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
+        <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
