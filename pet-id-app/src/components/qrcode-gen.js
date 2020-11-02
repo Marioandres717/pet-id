1507ec6301aafc87787e104732009eaced4fa56c
Mario Andres <marioarendon2@gmail.com>
2020-11-01 21:53:21 -0600

    add qrcode generation

---
+++ pet-id-app/src/components/qrcode-gen.js
@@ -0,0 +1,6 @@
+import React from "react"
+import QRCode from "qrcode.react"
+
+const QRCodeGen = () => <QRCode value="http://facebook.github.io/react/" />
+
+export default QRCodeGen
--- pet-id-app/src/pages/index.js
+++ pet-id-app/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import QRCodeGen from "../components/qrcode-gen"
 
 const Index = () => {
   return (
@@ -7,6 +8,7 @@ const Index = () => {
       <h1>This App rules!</h1>
       Log in to find out why
       <Link to="/dashboard"> Go to the dashboard</Link>
+      <QRCodeGen />
     </>
   )
 }
