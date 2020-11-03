917d2a3adb50c98bcd1fe3eee3c94cd3070c53c3
Mario Andres <marioarendon2@gmail.com>
2020-11-03 13:51:01 -0600

    check on 404 page if the browser window has loaded

--- pet-id-app/src/pages/404.js
+++ pet-id-app/src/pages/404.js
@@ -1,10 +1,18 @@
 import React from "react"
 
-const NotFoundPage = () => (
-  <>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const NotFoundPage = () => {
+  const browser = typeof window !== "undefined" && window
+
+  return (
+    <>
+      {browser && (
+        <>
+          <h1>404: Not Found</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </>
+      )}
+    </>
+  )
+}
 
 export default NotFoundPage
