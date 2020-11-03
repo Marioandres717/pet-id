a4e997c73730b41f2f4d210ade64316d1cf496ee
Mario Andres <marioarendon2@gmail.com>
2020-11-03 16:49:50 -0600

    Fetch pet data

--- pet-id-app/src/pages/app.js
+++ pet-id-app/src/pages/app.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { gql } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 
 const PET_BY_UUID = gql`
   query PetsByUuid($uuid: uuid!) {
@@ -30,7 +30,29 @@ const PET_BY_UUID = gql`
     }
   }
 `
-const App = () => {
-  return <div>HERE GOES THE READONLY DATA</div>
+const App = ({ location }) => {
+  const uuid = location.pathname.split("/app/")[1]
+  const { loading, error, data } = useQuery(PET_BY_UUID, {
+    variables: { uuid },
+  })
+
+  if (loading) return <p>LOADING....</p>
+  if (error) return <p>Error</p>
+
+  if (data.pets.length) {
+    const [pet] = data.pets
+    return (
+      <>
+        <h3>
+          {pet.id} - {pet.uuid}
+        </h3>
+        <p>{pet.name}</p>
+        <p>{pet.description}</p>
+        <p>{pet.species}</p>
+      </>
+    )
+  }
+
+  return <h1>NOTHING FOUND</h1>
 }
 export default App
