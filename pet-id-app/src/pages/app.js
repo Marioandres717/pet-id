876faf22baa4f1934d31bb64a67ea8acfdb0345f
Mario Andres <marioarendon2@gmail.com>
2020-11-03 14:47:05 -0600

    Add petsByUuid query

--- pet-id-app/src/pages/app.js
+++ pet-id-app/src/pages/app.js
@@ -1,4 +1,35 @@
 import React from "react"
+import { gql } from "@apollo/client"
+
+const PET_BY_UUID = gql`
+  query PetsByUuid($uuid: uuid!) {
+    pets(where: { uuid: { _eq: $uuid } }) {
+      id
+      name
+      description
+      species
+      uuid
+      avatar
+      user_pets {
+        user {
+          id
+          name
+          phone
+          avatar
+          email
+          address {
+            id
+            line_1
+            zip_or_postcode
+            province_or_state
+            city
+            country
+          }
+        }
+      }
+    }
+  }
+`
 const App = () => {
   return <div>HERE GOES THE READONLY DATA</div>
 }
