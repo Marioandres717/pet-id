a50996be00c77570f6587ed8a2698dedd2c05e21
Mario Andres <marioarendon2@gmail.com>
2020-11-18 22:46:48 -0600

    add Manifest File

--- pet-id-app/gatsby-config.js
+++ pet-id-app/gatsby-config.js
@@ -5,5 +5,21 @@ module.exports = {
     description: `CS476 Project, University of Regina. Pet Id Project: Identification for pets using QRCODE`,
     author: `@marioandres717`,
   },
-  plugins: ["gatsby-plugin-eslint", "gatsby-theme-apollo"],
+  plugins: [
+    "gatsby-plugin-eslint",
+    "gatsby-theme-apollo",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Pet Identification",
+        short_name: "Pet Id",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "src/images/icon.png",
+        crossOrigin: "use-credentials",
+      },
+    },
+  ],
 }
