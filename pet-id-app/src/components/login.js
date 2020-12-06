5f590e9ff2b83fcd2fb45588215fff1258f0cd0a
Mario Andres <marioarendon2@gmail.com>
2020-12-05 20:42:26 -0600

    Check isConfirmedUser on login component

--- pet-id-app/src/components/login.js
+++ pet-id-app/src/components/login.js
@@ -3,10 +3,22 @@ import { useIdentityContext } from "react-netlify-identity"
 import { navigate } from "gatsby"
 
 const Login = ({ showModal }) => {
-  const identity = useIdentityContext()
+  const { isLoggedIn, isConfirmedUser } = useIdentityContext()
 
-  if (identity && identity.isLoggedIn) {
-    navigate("/dashboard/profile", { replace: true })
+  if (isLoggedIn) {
+    if (!isConfirmedUser) {
+      return (
+        <>
+          <pre style={{ backgroundColor: "papayawhip" }}>
+            You have not confirmed your email. Please confirm it before you use
+            the site.
+          </pre>
+          <button>Verify</button>
+        </>
+      )
+    } else {
+      navigate("/dashboard/profile", { replace: true })
+    }
   }
 
   return (
