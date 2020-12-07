ef92926c9fec02f6eab8c1501dea04644d52d033
Mario Andres <marioarendon2@gmail.com>
2020-12-07 00:46:21 -0600

    Edit sw notification

--- pet-id-app/static/custom-sw-code.js
+++ pet-id-app/static/custom-sw-code.js
@@ -8,15 +8,8 @@ self.addEventListener("push", function (e) {
       dateOfArrival: Date.now(),
       primaryKey: "2",
     },
-    actions: [
-      {
-        action: "explore",
-        title: "Explore this new world",
-        icon: "images/checkmark.png",
-      },
-      { action: "close", title: "Close", icon: "images/xmark.png" },
-    ],
+    actions: [{ action: "close", title: "Close", icon: "images/xmark.png" }],
   }
 
-  self.registration.showNotification("Your pet was tag was scanne!", options)
+  self.registration.showNotification("Your pet tag was scanned!", options)
 })
